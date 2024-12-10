1596d1d593574c531bf43c2a2117a3be1662bc20
Vovka96 <s.vovan.yu@gmail.com>
2024-12-10 22:23:15 +0200

    change script

--- script.js
+++ script.js
@@ -2,22 +2,73 @@ let productsGrid = document.getElementById('product-grid');
 let productsArray = [];
 let url = 'https://my-json-server.typicode.com/Vovka96/marketplace';
 
-fetch (url + '/products')
-    .then (async function(response) {
-        let products = await response.json()
-        productsGrid.innerHTML = null;
-        products.forEach(p => {
-            productsArray.push(p);
-            let pElem = document.createElement('div');
-            pElem.classList.add('product')
-            pElem.innerHTML = `
+xhr.open('GET',url + '/products');
+xhr.responseType = 'json'
+xhr.onload = function() {
+    productsArray = xhr.response
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsArray.push(p);
+        let pElem = document.createElement('div');
+        pElem.classList.add('product')
+        pElem.innerHTML = `
             <h2 class='product-name>${p.name}</h2>
             <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
             <p class='product-price'><b>Price: </b>${p.price}$</p>
             <p class='product-description'><b>Description: </b>${p.description}</p>
             <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
             <button>Buy</button>
-            `;
-            productsGrid.append(pElem);
-        });
+        `;
+         productsGrid.append(pElem);
+    });
+}
+xhr.send();
+
+let cartProd = document.getElementById('cart-products');
+
+let cart = [];
+if(localStorage.getItem('cart')) {
+    cart.JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p.id == id;
     })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function(){
+        document.getElementById('cart-button').classList.remove('active');
+    },500);
+}
+
+function drawCartProducts() {
+    if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.price;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    localStorage.setItem("cart", '[]');
+}
+
+function openCart() {
+    cartProd.classList.toggle('hide');
+}
